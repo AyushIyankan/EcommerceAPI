9a1c0181073d2dec1cf490ee47a979f4820a505d
Ayush Iyankan <ayushiyankan007@gmail.com>
2023-02-21 17:38:44 +0530

    changes

--- app.js
+++ app.js
@@ -6,12 +6,30 @@ const app = express();
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
 
 const connectDB = require("./db/connect");
 const NotFound = require("./middleware/not-found");
 const ErrorHandler = require("./middleware/error-handler");
 
 const router = require("./routes");
+
+app.use("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+    message: "Too many requests, please try again in an hour",
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use(morgan("tiny"));
 app.use(express.json());
